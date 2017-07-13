19adc1c78fb3939879afe125daed460a78603144
efarfan2393 <e.farfanvaldivia@gmail.com>
2017-07-13 17:47:52 -0500

    learn arrow function

--- v02/main.js
+++ v02/main.js
@@ -6,12 +6,22 @@
 // let altura = 7;
 // console.log(`El área de un triángulo de base ${base} y altura ${altura} es:  ${base*altura/2}`);
 
+
+// const ---> no se puede cambiar el valor de la variable;
+
 let base = 5
 let height = 7
 
 function triangleArea(base, height){
 	return base * height / 2
 }
+console.log(`El área de un triángulo de base 
+	${base} y altura ${height} es:  ${triangleArea(base, height)}`);
 
+
+// Arrow function
+let base = 5
+let height = 7
+let triangleArea = (base, height) => base*height/2
 console.log(`El área de un triángulo de base 
 	${base} y altura ${height} es:  ${triangleArea(base, height)}`);
