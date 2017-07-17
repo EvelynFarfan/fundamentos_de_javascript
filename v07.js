ac0f1e4d1343f9e7eee9c2e22378f0e0bea7055f
efarfan2393 <e.farfanvaldivia@gmail.com>
2017-07-17 09:33:48 -0500

    v07

---
+++ v07.js
@@ -0,0 +1,20 @@
+const born = new Date (1993, 8, 12)
+const hoy = new Date ()
+const tiempo = hoy - born
+const tiempoSegundos = tiempo / 1000
+const tiempoMin = tiempoSegundos / 60
+const tiempoHoras = tiempoMin / 60
+
+
+
+const nextBirthday = new Date(hoy.getFullYear(), born.getMonth(), born.getDate())
+
+const diasSemana = [
+	'Domingo',
+	'Lunes',
+	'Martes',
+	'Miercoles',
+	'Jueves',
+	'Viernes',
+	'Sábado'
+]
