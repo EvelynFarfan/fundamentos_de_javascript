178ef1fa5bebe9633b6b297fa0b4ba141b5411f7
efarfan2393 <e.farfanvaldivia@gmail.com>
2017-07-25 15:17:57 -0500

    v26

--- Ejercicios V/v26.js
+++ Ejercicios V/v26.js
@@ -51,3 +51,45 @@ get('http://www.swapi.co/api/people/1/')
 		console.log(`${luke.name} nació en ${luke.homeworld.name}`)
 	})
 	.catch(err => handleError(err))
+
+// FETCH - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+function get(URL){
+	return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+
+		xhr.onreadystatechange = function () {
+			const DONE = 4
+			const OK = 200
+			if (this.readyState === DONE) {
+				if(this.status === OK){
+					// resolve significa que se hizo todo bien
+					resolve(JSON.parse(this.responseText))
+				}else {
+					//reject solo enviar el error
+					reject(new Error(`Se produjo un error al realizar el request ${this.status}`))
+				}
+			}
+		}
+		xhr.open('GET', URL);
+		xhr.send(null);
+	})
+}
+/* Funcion para tratar el error */
+function handleError(err){
+	console.log(`Request failed: ${err}`)
+}
+let luke
+fetch('https://www.swapi.co/api/people/1/')
+	.then(response => response.json())
+	.then((json) => {
+		luke = json
+		return fetch(luke.homeworld)
+	})
+	.then(response => response.json())
+	.then((json) => {
+		luke.homeworld = json
+		console.log(`${luke.name} nació en ${luke.homeworld.name}`)
+	})
+	.catch(err => handleError(err))
+         
